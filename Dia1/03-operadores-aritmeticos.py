b40527005808ec19c6d555c2ea50dcf4df9d9c42
stevejesuspuenteblanco <stevejesuspuenteblanco@hotmail.com>
2022-03-01 21:36:27 -0500

    control flujo

--- Dia1/03-operadores-aritmeticos.py
+++ Dia1/03-operadores-aritmeticos.py
@@ -1,24 +1,29 @@
-numero1=10
-numero2=20
+numero1 = 10
+numero2 = 20
 
-#SUMA
-print(numero1+numero2)
-#RESTA
-print(numero1-numero2)
-#MULTIPLICACION
-print(numero1*numero2)
-#DIVISION
-print(numero1/numero2)
-#MODULO
-print(numero1%numero2)
-#COCIENTE
-print(numero1//numero2)
+# SUMA +
+print( numero1 + numero2) # 30
+# RESTA - 
+print( numero1 - numero2) # -10
+# MULTIPLICACION *
+print( numero1 * numero2) # 200
+# DIVISION /
+print(numero1 / numero2) # 0.5
+# MODULO %
+print(numero1 % numero2) # 10
+# COCIENTE //
+print(numero1 // numero2) # 0
 
 nombre= 'eduardo'
 ciudad= 'arequipa'
 
-#para la suma al ser str se hara una concatenacion (se fusionara los valores)
-print(nombre+ ciudad)
-print(nombre * 2)
-
+# EN EL CASO DE LOS STRINGS: 
+# Para la suma al ser str se hara una concatenacion (se fusionara los valores)
+print(nombre + ciudad)
+# en el caso de la multiplicacion al ser entre un str y un int lo que se hara se mostrara la cantida de veces repetidas indicadas en el int del str
 print(nombre * 10)
+
+# No esta permitido entre str (solo numericos como int y float)
+# print(nombre - ciudad)
+# print(nombre / ciudad)
+# print(nombre % ciudad)
