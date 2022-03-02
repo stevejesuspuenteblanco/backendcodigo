b5da3ad25704a38345fd718054efbfb23eb03727
stevejesuspuenteblanco <stevejesuspuenteblanco@hotmail.com>
2022-03-01 23:03:38 -0500

    notas practica

--- Dia2/03-bucle-for.py
+++ Dia2/03-bucle-for.py
@@ -6,3 +6,20 @@ notas = [10,20,16,8,6,1]
 
 for nota in notas:
     print(nota)
+
+#crearemos un bucle manual para una iteracion con limite 10
+for numero in range(10):
+    print(numero)
+
+#con dos parametros el primero es donde inicia y el segundo donde termina
+for numero in range(5,10):    
+    print(numero)
+
+
+#si colocamos dos parametros el primeri es numero inicial, el segundo el que termina y 
+#el tercero en cuanto hara la decrementacion
+for numero in range(5,10,2):
+    print(numero)
+
+#imprimir los tres valores iniciales de notas
+
---
+++ Dia2/practica2.py
@@ -0,0 +1,7 @@
+notas = [10,20,16,8,6,1]
+for nota in notas[:3]:
+    print(nota)
+
+#for posicion in range(3):
+#  print(notas[posicion])
+#
